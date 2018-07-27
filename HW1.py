3bc0739bea4db47d872180ec5e04885f47d65a8b
Miles McCall <mccallm@oregonstate.edu>
2018-07-27 00:36:58 -0700

    table reformatting

--- HW1.py
+++ HW1.py
@@ -17,14 +17,14 @@ def sql_init():
     print(mydb)
     mycursor = mydb.cursor()
 
-    # Remove old versions
+    print("Removing old versions...")
     mycursor.execute("DROP TABLE IF EXISTS keywords")
-    mycursor.execute("DROP TABLE IF EXISTS companies")
-    mycursor.execute("DROP TABLE IF EXISTS countries")
-    mycursor.execute("DROP TABLE IF EXISTS languages")
+    mycursor.execute("DROP TABLE IF EXISTS production_companies")
+    mycursor.execute("DROP TABLE IF EXISTS production_countries")
+    mycursor.execute("DROP TABLE IF EXISTS original_language")
     mycursor.execute("DROP TABLE IF EXISTS movies")
 
-    # Make tables
+    print("Generating table schema...")
     mycursor.execute("""CREATE TABLE IF NOT EXISTS movies (
         id INT PRIMARY KEY,
         title VARCHAR(255),
@@ -44,16 +44,6 @@ def sql_init():
         )"""
     )
 
-    mycursor.execute("""CREATE TABLE IF NOT EXISTS genres (
-        id INT PRIMARY KEY,
-        movie_id INT,
-        INDEX movie_ind (movie_id),
-        FOREIGN KEY (movie_id)
-            REFERENCES movies(id),
-        name VARCHAR(255)
-        )"""
-    )
-
     mycursor.execute("""CREATE TABLE IF NOT EXISTS keywords (
         id INT PRIMARY KEY,
         movie_id INT,
@@ -64,7 +54,7 @@ def sql_init():
         )"""
     )
 
-    mycursor.execute("""CREATE TABLE IF NOT EXISTS companies (
+    mycursor.execute("""CREATE TABLE IF NOT EXISTS production_companies (
         id INT PRIMARY KEY,
         movie_id INT,
         INDEX movie_ind (movie_id),
@@ -74,7 +64,7 @@ def sql_init():
         )"""
     )
 
-    mycursor.execute("""CREATE TABLE IF NOT EXISTS countries (
+    mycursor.execute("""CREATE TABLE IF NOT EXISTS production_countries (
         iso_3166_1 INT PRIMARY KEY,
         movie_id INT,
         INDEX movie_ind (movie_id),
@@ -84,7 +74,7 @@ def sql_init():
         )"""
     )
 
-    mycursor.execute("""CREATE TABLE IF NOT EXISTS languages (
+    mycursor.execute("""CREATE TABLE IF NOT EXISTS original_language (
         iso_639_1 INT PRIMARY KEY,
         movie_id INT,
         INDEX movie_ind (movie_id),
@@ -94,47 +84,70 @@ def sql_init():
         )"""
     )
 
+    mycursor.execute("""CREATE TABLE IF NOT EXISTS genres (
+        id INT PRIMARY KEY,
+        movie_id INT,
+        INDEX movie_ind (movie_id),
+        FOREIGN KEY (movie_id)
+            REFERENCES movies(id),
+        name VARCHAR(255)
+        )"""
+    )
+
 def read_csv():
+    tables = ["movies","keywords","production_companies","production_countries","original_language","genres"]
+    print("Parsing CSV file...")
     with open('./tmdb_5000_movies.csv', 'r') as csvfile:
+
         spamreader = csv.reader(csvfile, delimiter=',')
         header = next(spamreader)
-        print(header)
+        #print(header)
         rowCount = 0
 
         for row in spamreader:
-            print('------------------------------------------')
-            headerIndex = 0
 
-            #This is one way to iterate through all of the attribute columns with their corresponding headers
+            print('------------------------------------------')
+
+            headerIndex = 0
             for attributeName in header:
                 print(attributeName)
-                print('\t' + row[headerIndex])
+                for col in tables:
+                    if col == attributeName:
+                        print("\t ",col)
+                        break
+                    else:
+                        print("\t movies")
+
+                #print('\t' + row[headerIndex])
                 headerIndex += 1
-                print()
+
             print('------------------------------------------')
-            #This is one way to parse the dictionaries in the CSV file
+
             #List of dictionaries
-            print('Parsed Line')
-            parsedLine = json.loads(row[1])
-            print(parsedLine)
-            print('------------------------------------------')
+            #print('Parsed Line')
+            #parsedLine = json.loads(row[1])
+            #print(parsedLine)
+
+            #print('------------------------------------------')
 
             #First dictionary
-            print('First Dictionary')
-            print(parsedLine[0])
-            print('------------------------------------------')
+            #print('First Dictionary')
+            #print(parsedLine[0])
+
+            #print('------------------------------------------')
 
             #Key and values
-            print('Key and Values')
-            print()
-            for key in parsedLine[0]:
-                print('Key: ' + key)
-                print('Value: ' + str(parsedLine[0][key]))
-                print()
-            print('------------------------------------------')
-            if rowCount > 1:
-                break
+            #print('Key and Values')
+            #print()
+            #for key in parsedLine[0]:
+                #print('Key: ' + key)
+                #print('Value: ' + str(parsedLine[0][key]))
+                #print()
+
+            #print('------------------------------------------')
+
             rowCount += 1
+            break
 
 def sql_inserts():
     mydb = mysql.connector.connect(
@@ -148,7 +161,7 @@ def sql_inserts():
 
 def main():
     sql_init()
-    sql_inserts()
+    #sql_inserts()
     read_csv()
 
 if __name__ == '__main__':
